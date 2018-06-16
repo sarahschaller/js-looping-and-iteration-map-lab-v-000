80ffd834e84b741524689cd8334c49ede077333a
Sarah Schaller <sarahcschaller@gmail.com>
2018-06-16 18:45:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function lowerCaseDrivers(drivers) {
-  return drivers.map(function(drivers){
+  return drivers.map(function(drivers) {
     return drivers.toLowerCase();
+  });
+}
+function nameToAttributes(drivers) {
+  return drivers.map(function(name) {
+    const driverFirstName = name.split(' ')[0];
+    const driverLastName = name.split(' ')[1];
+    return {firstName: driverFirstName, lastName: driverLastName};
   });
 }
